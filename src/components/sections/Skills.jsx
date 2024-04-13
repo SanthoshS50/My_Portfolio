adfba303b27d699c7de83945f6bb65a21233a4a1
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-04-13 20:14:32 +0530

    Skills Updated

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -127,8 +127,7 @@ const Skills = () => {
             marginBottom: "40px",
           }}
         >
-          Here are some of my skills on which I have been working on for the
-          past 3 years.
+          Here are some of my skills on which I have been working on...
         </Desc>
 
         <SkillsContainer>
