a2b86549415cd3866611bea837430ee05bb2a7eb
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-06-05 11:42:47 +0530

    Resume Updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SanthoshS50",
   resume:
-    "https://drive.google.com/file/d/1Ebtd2zcjkCPCVy4nM8KbjNWJLuW0Jebw/view?usp=sharing",
+    "https://drive.google.com/file/d/1REWVxis9EZE4ytZqHeJ2ninfPLYBDH2u/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/santhosh29092001/",
   youtube:"https://www.youtube.com/@wonderaquagarden",
   insta: "https://www.instagram.com/__.sweet._.sandy._.official.__/",
