4a515250cdd44510a32e4b8f3c87d1fc6d3d0e83
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-09-16 13:28:14 +0530

    Hero Image Updated

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Bio } from "../../data/constants";
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/HeroImage.jpg";
+import HeroImg from "../../images/HeroImage.jpeg";
 import HeroBgAnimation from "../HeroBgAnimation";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
